38f794fc3e4af84c99fcdf4dabfe830ca96f44c9
Richardel Leang <richardel.leang@dawsoncollege.qc.ca>
2025-12-19 11:48:11 -0500

    test redirection 6

--- fakeIndex.php
+++ fakeIndex.php
@@ -1,3 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form response</title>
+    </head>
+    <body>
+        <p><?= var_dump($_GET) ?> </p>
+        <p><?= var_dump($_POST) ?></p>
 
-?>
+        <p>
+        <?php=
+        $servername = "thelichking"; // keep $servername to localhost if doing it on own Pi
+        $username = "Arthas";
+        $password = "Shell111";
+        $dbname = "unitStats";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+        // Check for successful connection
+        if (!$conn) { 
+            die("Connection failed: {mysqli_connect_error()}"); 
+        } 
+        echo "Connected successfully";
+        
+        $sql = "select * from unitList;";		// This can be any valid SQL cmd.
+        $result = mysqli_query($conn, $sql);	// Stores rows retrieved by query.
+        echo mysqli_error($conn);	    		// If error, this determines cause.
+        
+
+
+        mysqli_close($conn);
+        ?>
+        </p>
+
+
+        <script>
+			function google() {
+				window.location.replace("https://www.google.com/" + $_GET["search"]);
+			}
+		</script>
+    </body>
+</html>
